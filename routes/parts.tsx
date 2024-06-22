0ee8b1cc3a9224dbbeee4ccc2f5009303fa06028
Jordan Reger <mail@jordanreger.com>
2024-06-22 12:05:07 -0400

    add styles, readd dialogs

--- routes/parts.tsx
+++ routes/parts.tsx
@@ -3,7 +3,6 @@
 import { supabase } from "@/tools/supabase.tsx";
 import { Tables } from "@/tools/database.types.tsx";
 import { Header } from "@/islands/Header.tsx";
-import { PartButton } from "@/components/PartButton.tsx";
 
 type Part = {
   part: string;
@@ -20,13 +19,40 @@ export default async function Parts() {
   //console.log(data);
   const output: preact.VNode[] = [];
   data.forEach((part: Tables<"parts">) => {
+    let key: keyof Part;
+
+    const detailedPartList: preact.VNode[] = [];
+
+    for (key in part) {
+      detailedPartList.push(
+        <p>
+          <small class="small-heading">
+            {key.toLocaleUpperCase()}
+          </small>
+          <br />
+          <input value={part[key]?.toString()} required />
+        </p>,
+      );
+    }
+
     output.push(
       <tr>
         <td>{part.type}</td>
         <td>{part.value}</td>
         <td>{part.footprint}</td>
         <td>
-          <PartButton />
+	<button class="dialog-open" id={`dialog-open-${data.indexOf(part)}`}>
+	    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="000" class="editicon"><path d="M216-216h51l375-375-51-51-375 375v51Zm-72 72v-153l498-498q11-11 23.84-16 12.83-5 27-5 14.16 0 27.16 5t24 16l51 51q11 11 16 24t5 26.54q0 14.45-5.02 27.54T795-642L297-144H144Zm600-549-51-51 51 51Zm-127.95 76.95L591-642l51 51-25.95-25.05Z"/></svg>
+	  </button>
+          <dialog id={`dialog-${data.indexOf(part)}`}>
+            <form>
+              {detailedPartList}
+            </form>
+
+            <button class="dialog-close" id={`dialog-close-${data.indexOf(part)}`}>
+              Close
+            </button>
+          </dialog>
         </td>
       </tr>
     );
@@ -60,6 +86,7 @@ export default async function Parts() {
           </tr>
         </table>
       </div>
+      <script src="partDialogs.js" type="module" />
     </>
   );
 }
