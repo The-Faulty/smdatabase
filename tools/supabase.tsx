411521aedec1688bdcc72e0c0cc7eb3c8f2b72f0
The-Faulty <andydandy4001@gmail.com>
2024-06-27 22:49:20 -0400

    revert supabase key changes

--- tools/supabase.tsx
+++ tools/supabase.tsx
@@ -2,8 +2,19 @@ import { createClient } from "npm:@supabase/supabase-js";
 import { Database } from "@/tools/database.types.tsx";
 import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
-const env = await load();
-const supabaseKey: string = Deno.env.get("REACT_APP_SUPABASE_ANON_KEY")!;
-const supabaseUrl: string = Deno.env.get("REACT_APP_SUPABASE_URL")!;
+const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
+
+var key,
+  url: string = "";
+if (!isDenoDeploy) {
+  const env = await load();
+  key = env["REACT_APP_SUPABASE_ANON_KEY"];
+  url = env["REACT_APP_SUPABASE_URL"];
+} else {
+  key = Deno.env.get("REACT_APP_SUPABASE_ANON_KEY")!;
+  url = Deno.env.get("REACT_APP_SUPABASE_URL")!;
+}
+const supabaseKey: string = key;
+const supabaseUrl: string = url;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
